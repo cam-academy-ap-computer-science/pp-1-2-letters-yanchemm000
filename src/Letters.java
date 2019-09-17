7c6a5f3322693f45ae599f1a0ab5dabfa90698c1
yanchemm000 <yanchemm000@CAM-LAB-29.bgsd.k12.wa.us>
2019-09-17 07:37:49 -0700

    I'm done

--- src/Letters.java
+++ src/Letters.java
@@ -3,7 +3,62 @@ public class Letters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		letter1();
+		System.out.println();
+		letter2();
+		System.out.println();
+		letter3();
+	
 	}
+	public static void start() {
+		System.out.print("Dear ");
+	}
+	public static void collagefriends() {
+		System.out.println("Hey! Only a couple more days until you start school");
+	}
+	public static void school() {
+		System.out.println("School's been fine for me. In fact, I'm writing this letter because of an assignmnet.");
+	}
+	public static void secretmessage() {
+		System.out.println("Veru sa spun un secretu care nicuna puota se cetesc.");
+	}
+	public static void bigfoot() {
+		System.out.println("While you're up and traveling, keep an eye out for Bigfoot.");
+		System.out.println("I haven't seen him in a while, and i'm growing concerned.");
+	}
+	public static void end() {
+		System.out.println("Te ubesc,");
+		System.out.println("Emmy");
+	}
+	public static void otherend() {
+		System.out.println("Your partner is crime,");
+		System.out.println("Ivory Coco");
+	}
+	public static void letter1() {
+		start();
+		System.out.println("Rock");
+		collagefriends();
+		school();
+		secretmessage();
+		otherend();
+	}
+	public static void letter2() {
+		start();
+		System.out.println("Roll");
+		school();
+		secretmessage();
+		bigfoot();
+		otherend();
+	}
+	public static void letter3() {
+		start();
+		System.out.println("MJ");
+		collagefriends();
+		school();
+		secretmessage();
+		bigfoot();
+		end();
+	}
+	
 
 }
